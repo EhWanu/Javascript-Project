4b225795f546c567b3edfdcbfe238ebab1911acc
EhWanu <77857074+EhWanu@users.noreply.github.com>
2021-01-28 17:09:59 -0500

    Removed old code,  added first Pokemon too list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
-alert('Hello world');
-let  favouriteFood='Pizza';
-document.write(favouriteFood)
+let pokemonList= [
+  {
+    id: 001,
+  name: 'Bulbasaur',
+  number: '1',
+  height: '0.7m',
+  weight: '6.9kg',
+  type: 'Monster, Grass',
+}
+{
+
+  id:004,
+  name: 'Charmander',
+  number: '4',
+  height: '0.6m',
+  weight: '8.5kg',
+  type: 'Monster , Dragon'
+}
+{
+  id:'007',
+  name: 'Squirtle',
+  number: '7',
+  height: '0.5m',
+  weight: '9kg',
+  type: 'Mosnter , Water',
+}
+]
---
+++ test.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <title>Javascript Project</title>
+  </head>
+  <body>
+<script src="js/scripts"></script>
+  </body>
+</html>

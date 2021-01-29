d01bfc282de8f00792b3a149f8bfb5840ffc7fc4
EhWanu <77857074+EhWanu@users.noreply.github.com>
2021-01-29 13:34:31 -0500

    Ficed height characters and added if else LOOP

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ let pokemonList= [
     id: 001,
   name: 'Bulbasaur',
   number: '1',
-  height: '0.7m',
+  height: 0.7,
   weight: '6.9kg',
   type: 'Monster, Grass',
 },
@@ -11,7 +11,7 @@ let pokemonList= [
   id:004,
   name: 'Charmander',
   number: '4',
-  height: '0.6m',
+  height: 0.6,
   weight: '8.5kg',
   type: 'Monster , Dragon',
 },
@@ -19,8 +19,19 @@ let pokemonList= [
   id:'007',
   name: 'Squirtle',
   number: '7',
-  height: '0.5m',
+  height: 0.5,
   weight: '9kg',
   type: 'Mosnter , Water',
 },
 ];
+
+
+for (let i=0; i <pokemonList.length; i++)
+{
+  if (pokemonList[i].height >= 0.7) //if Pokemon is equal too or larger than 0.7
+  document.write('<br>' + pokemonList[i].id + ' ' + pokemonList[i].name + ' height: ' + pokemonList[i].height + ' -  This one is the biggest! '); //for loop writing pokedex attributes.
+
+
+  else if (pokemonList[i].height < 0.7) //if pokemon is smaller than 0.7
+  document.write('<br>' + pokemonList[i].id + ' ' + pokemonList[i].name + ' height: ' + pokemonList[i].height);  //for loop writing pokedex attributes.
+}
